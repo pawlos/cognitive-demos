8a72e31db8df010c904e33e8711b85223abfa287
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-02-11 16:06:37 +0100

    Added initial Vision view

---
+++ WpfApplication1/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApplication1.Plumbing;
+
+namespace WpfApplication1
+{
+    class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            ExecuteView = new RelayCommand(p => true, Execute);
+        }
+        public ICommand ExecuteView { get; set; }
+
+        private void Execute(object parameter)
+        {
+            
+        }
+    }
+}
